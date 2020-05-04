1fca8b0cd9da60a363c9bd2d92d7786d86129fd0
Mathilde Chazaud <64693696+MathildeChazaud@users.noreply.github.com>
2020-05-04 11:26:19 +0200

    Update ui.R

--- data-products-week-4-project-master/source/ui.R
+++ data-products-week-4-project-master/source/ui.R
@@ -152,7 +152,7 @@ shinyUI(
              
       h3("Developing Data Products course - Assignment Week 4 - Shiny Application and Reproducible Pitch"),
       
-      h3("Author: Gaston Dario Napoli - March, 2017"),
+      h3("Author: Mathilde Chazaud - May, 2020"),
       
       br(),
       
@@ -163,8 +163,8 @@ shinyUI(
       ),
       
       p("Source code of this application is available at",
-        a(href = "https://github.com/laplata2003/data-products-week-4-project",
-          "https://github.com/laplata2003/data-products-week-4-project")
+        a(href = "https://github.com/MathildeChazaud/Developping-Data-Product/edit/master/data-products-week-4-project-master",
+          "https://github.com/MathildeChazaud/Developping-Data-Product/edit/master/data-products-week-4-project-master")
       )
       
     )
